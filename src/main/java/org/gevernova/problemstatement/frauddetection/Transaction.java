32ef3a58d6b0e257520c34d76457d39ded1025bb
XEROX112 <jayabhardwaz1@gmail.com>
2026-01-28 23:37:04 +0530

    [Aryan] :Added Transaction Entity

---
+++ src/main/java/org/gevernova/problemstatement/frauddetection/Transaction.java
@@ -0,0 +1,17 @@
+package org.gevernova.problemstatement.frauddetection;
+
+
+class Transaction {
+    int id;
+    String policy;
+    double amount;
+    boolean fraud;
+
+    Transaction(int id,String policy,double amount,boolean fraud){
+        this.id=id;
+        this.policy=policy;
+        this.amount=amount;
+        this.fraud=fraud;
+    }
+}
+
