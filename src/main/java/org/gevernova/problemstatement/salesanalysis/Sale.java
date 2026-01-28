14ffd64e10ae82116ada433ead79948100b18312
XEROX112 <jayabhardwaz1@gmail.com>
2026-01-28 23:47:23 +0530

    [Aryan] :Added Sale Entity

---
+++ src/main/java/org/gevernova/problemstatement/salesanalysis/Sale.java
@@ -0,0 +1,14 @@
+package org.gevernova.problemstatement.salesanalysis;
+
+class Sale {
+    int productId;
+    int quantity;
+    double price;
+
+    Sale(int productId,int quantity,double price){
+        this.productId=productId;
+        this.quantity=quantity;
+        this.price=price;
+    }
+}
+
