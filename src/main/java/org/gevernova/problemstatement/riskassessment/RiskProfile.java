078d194786b0cf43821bbf41f3c283bf5df03197
XEROX112 <jayabhardwaz1@gmail.com>
2026-01-28 23:41:50 +0530

    [Aryan] :Added RiskProfile Entity

---
+++ src/main/java/org/gevernova/problemstatement/riskassessment/RiskProfile.java
@@ -0,0 +1,18 @@
+package org.gevernova.problemstatement.riskassessment;
+
+
+class RiskProfile {
+    int id;
+    String name;
+    double score;
+
+    RiskProfile(int id,String name,double score){
+        this.id=id;
+        this.name=name;
+        this.score=score;
+    }
+
+    public String toString(){
+        return id+" "+name+" "+score;
+    }
+}
