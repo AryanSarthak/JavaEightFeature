7e4cbe34e6a479420a1a42d4fbea45148f42d9d0
XEROX112 <jayabhardwaz1@gmail.com>
2026-01-28 23:42:40 +0530

    [Aryan] :Added PolicyHolder Entity

---
+++ src/main/java/org/gevernova/problemstatement/riskassessment/PolicyHolder.java
@@ -0,0 +1,19 @@
+package org.gevernova.problemstatement.riskassessment;
+
+
+class PolicyHolder {
+    int id;
+    String name;
+    int age;
+    String type;
+    double premium;
+
+    PolicyHolder(int id,String name,int age,String type,double premium){
+        this.id=id;
+        this.name=name;
+        this.age=age;
+        this.type=type;
+        this.premium=premium;
+    }
+}
+
