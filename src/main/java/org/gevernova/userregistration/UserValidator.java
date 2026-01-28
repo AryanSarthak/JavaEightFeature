577407ccf0b465e284cf28146d08369840dcd955
XEROX112 <jayabhardwaz1@gmail.com>
2026-01-28 21:45:34 +0530

    [Aryan] :Added User Validation

---
+++ src/main/java/org/gevernova/userregistration/UserValidator.java
@@ -0,0 +1,20 @@
+package org.gevernova.userregistration;
+
+public class UserValidator {
+
+    Validator firstName = name ->
+            name.matches("^[A-Z][a-z]{2,}$");
+
+    Validator lastName = name ->
+            name.matches("^[A-Z][a-z]{2,}$");
+
+    Validator email = mail ->
+            mail.matches("^[a-z]+([.+_-][a-z0-9]+)*@[a-z0-9]+\\.[a-z]{2,}(\\.[a-z]{2,})?$");
+
+    Validator mobile = mob ->
+            mob.matches("^[0-9]{2}\\s[0-9]{10}$");
+
+    Validator password = pass ->
+            pass.matches("^(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{8,}$");
+
+}
